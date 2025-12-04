0b4ee2625516cc0138260cdd3bf1f6e40d89f9f1
alraro <alfonsoramosrojas@gmail.com>
2025-12-05 00:15:18 +0100

    Added templates for all days in AdventOfCode

---
+++ Day11/main.py
@@ -0,0 +1,11 @@
+def main():
+	acum = 0
+	has_changed = True
+	input_file = "Day4/input"
+	# input_file = "Day 2/test"
+	with open(input_file, "r") as file:
+		lines = list(file.readlines())
+	
+	print("Total rolls:", acum)
+if __name__ == "__main__":
+	main()
