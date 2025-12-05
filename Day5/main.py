d129e831ab8e1456017e5db118bb241e2e20d3d1
alraro <alfonsoramosrojas@gmail.com>
2025-12-05 10:14:48 +0100

    Finished part 1 day 5

--- Day5/main.py
+++ Day5/main.py
@@ -1,11 +1,28 @@
+def parse_input(lines):
+	divider = lines.index("\n")
+	ranges = lines[:divider]
+	ingredients = lines[divider+1:]
+	ranges = list(map(lambda x: x.strip(), ranges))
+	ingredients = list(map(lambda x: x.strip(), ingredients))
+	return ranges, ingredients
+
+def is_fresh(ingredient, ranges):
+	ingredient = int(ingredient)
+	for range in ranges:
+		left, right = range.split('-')
+		if ingredient >= int(left) and ingredient <= int(right):
+			return True
+	return False
+
 def main():
 	acum = 0
-	has_changed = True
-	input_file = "Day4/input"
-	# input_file = "Day 2/test"
+	input_file = "Day5/input"
 	with open(input_file, "r") as file:
 		lines = list(file.readlines())
-	
-	print("Total rolls:", acum)
+	ranges, ingredients = parse_input(lines)
+	for ingredient in ingredients:
+		if is_fresh(ingredient, ranges):
+			acum += 1
+	print(f"Total fresh items are: {acum}")
 if __name__ == "__main__":
 	main()
