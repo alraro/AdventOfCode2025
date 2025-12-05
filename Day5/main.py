024c259380f20717b6e31c7fb1ac9620a3d03aa6
alraro <alfonsoramosrojas@gmail.com>
2025-12-05 10:51:18 +0100

    Finshed task 2 day 5

--- Day5/main.py
+++ Day5/main.py
@@ -14,15 +14,46 @@ def is_fresh(ingredient, ranges):
 			return True
 	return False
 
-def main():
+def get_fresh_ingredients(ranges, ingredients):
 	acum = 0
+	for ingredient in ingredients:
+		if is_fresh(ingredient, ranges):
+			acum += 1
+	return acum
+
+def get_all_fresh(ranges):
+	intervals = []
+	for r in ranges:
+		left, right = map(int, r.split('-'))
+		intervals.append((left, right))
+	intervals.sort(key=lambda x: x[0])
+ 
+	merged = []
+	if not intervals:
+		return 0
+	current_start, current_end = intervals[0]
+	for start, end in intervals[1:]:
+		if start <= current_end:
+			current_end = max(current_end, end)
+		else:
+			merged.append((current_start, current_end))
+			current_start, current_end = start, end
+
+	merged.append((current_start, current_end))
+
+	total_count = 0
+	for start, end in merged:
+		total_count += end - start + 1
+	return total_count
+  
+def main():
 	input_file = "Day5/input"
 	with open(input_file, "r") as file:
 		lines = list(file.readlines())
 	ranges, ingredients = parse_input(lines)
-	for ingredient in ingredients:
-		if is_fresh(ingredient, ranges):
-			acum += 1
-	print(f"Total fresh items are: {acum}")
+	ingredients_fresh = get_fresh_ingredients(ranges, ingredients)
+	all_possible_fresh = get_all_fresh(ranges)
+	print(f"Total fresh items are: {ingredients_fresh}")
+	print(f"The max number of fresh items is: {all_possible_fresh}")
 if __name__ == "__main__":
 	main()
