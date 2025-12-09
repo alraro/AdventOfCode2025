4f21f723e78d0d6ea7df74d4ff7d13119e730854
alraro <alfonsoramosrojas@gmail.com>
2025-12-09 15:19:29 +0100

    Added board class

--- Day9/main2.py
+++ Day9/main2.py
@@ -13,14 +13,32 @@ class Tile:
 		return (abs(self.x - other.x) + 1) * (abs(self.y - other.y) + 1)
 
 
-def correct(tiles, a, b):
-	1
+class Board:
+	def __init__(self):
+		self.tiles = []
+		self.count = 0
+  
+	def add_tile(self, tile):
+		self.tiles.append(tile)
+		self.count += 1
+  
+	def get_tile(self, index):
+		return self.tiles[index]
 
+	def is_inside(self, corner1, corner2):
+		x_min = min(corner1.x, corner2.x)
+		x_max = max(corner1.x, corner2.x)
+		y_min = min(corner1.y, corner2.y)
+		y_max = max(corner1.y, corner2.y)
+		for tile in self.tiles:
+			if x_min <= tile.x <= x_max and y_min <= tile.y <= y_max:
+				return True
+		return False
+ 
 def main():
-	tiles = []
+	board = Board()
 	biggest = 0
 	input_file = "Day9/input"
-	# input_file = "Day 2/test"
 	with open(input_file, "r") as file:
 		lines = list(file.readlines())
 	for line in lines:
@@ -28,13 +46,13 @@ def main():
 		x, y = line.split(",")
 		x = int(x)
 		y = int(y)
-		tiles.append(Tile(x, y))
+		board.add_tile(Tile(x, y))
 	i = 0
-	while i < len(tiles) - 1:
-		curr = tiles[i]
+	while i < board.count - 1:
+		curr = board.get_tile(i)
 		j = i + 1
-		while j < len(tiles):
-			other = tiles[j]
+		while j < board.count:
+			other = board.get_tile(j)
 			area = curr.get_area(other)
 			biggest = max(biggest, area)
 			j += 1
