78b41eaaf9ac94a1c3b58bf30f02098a512ab235
alraro <alfonsoramosrojas@gmail.com>
2025-12-10 22:10:04 +0100

    Added basic parsing day 10

--- Day10/main.py
+++ Day10/main.py
@@ -1,11 +1,17 @@
+def parse_machine(line):
+	parts = line.split(" ")
+	lights = list(map(lambda x: True if x == '#' else False, parts[0].strip('[]')))
+	buttons = list(map(int, parts[-1].strip('{}').split(',')))
+	joltages = list(map(lambda x: list(map(int, x.strip('()').split(','))), parts[1:-1]))
+	return lights, buttons, joltages
+
 def main():
 	acum = 0
-	has_changed = True
-	input_file = "Day4/input"
-	# input_file = "Day 2/test"
+	input_file = "Day10/test"
 	with open(input_file, "r") as file:
 		lines = list(file.readlines())
-	
-	print("Total rolls:", acum)
+	for line in lines:
+		lights, buttons, joltages = parse_machine(line.strip())
+		print(f"Lights: {lights}, Buttons: {buttons}, Joltages: {joltages}")
 if __name__ == "__main__":
 	main()
